ef15dd20da1c0a1b368230e8bbc452811dd27b65
Ainhi2005 <your_email@example.com>
2026-06-03 01:37:26 +0700

    feat<sinhvien>: create Sinhvien

--- app/views/sinhvien/create.php
+++ app/views/sinhvien/create.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h2>Thêm mới sinh viên </h2>
+    <form action="/QLSINHVIEN/public/sinhvien/store" method="post">
+        <label for="hoten">Họ tên</label>
+        <input type="text" name="hoten" id="hoten">
+        <br>
+        <label for="gioitinh">Giới tính</label>
+        <input type="text" name="gioitinh" id="gioitinh">
+        <br>
+        <label for="mssv">MSSV</label>
+        <input type="text" name="mssv" id="mssv">
+        <br>
+        <button type="submit">Thêm mới</button>
+    </form>
+
 </body>
+
 </html>
--- app/views/sinhvien/index.php
+++ app/views/sinhvien/index.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title ?></title>
-</head>
 <style>
     table, th, td {
         text-align: center;
@@ -18,26 +11,22 @@
         background-color: #456fc8;
         color: white;
     }
-
 </style>
-<body>
 
-    <h1><?php echo $title ?></h1>
-    <table >
-        <tr >
-            <th>id</th>
-            <th>Ho va ten</th>
-            <th>Gioi tinh</th>
-            <th>mssv</th>
+<h1><?php echo $title ?></h1>
+<table style="width:100%">
+    <tr>
+        <th>id</th>
+        <th>Ho va ten</th>
+        <th>Gioi tinh</th>
+        <th>mssv</th>
+    </tr>
+    <?php foreach ($sinhvien as $sv) { ?> 
+        <tr>
+            <td> <?php echo $sv['id']; ?> </td>
+            <td> <?php echo $sv['sinhvien']; ?> </td>
+            <td> <?php echo $sv['giotinh']; ?> </td>
+            <td> <?php echo $sv['mssv']; ?> </td>
         </tr>
-        <?php foreach ($sinhvien as $sv) { ?> 
-            <tr>
-                <td> <?php echo $sv['id']; ?> </td>
-                <td> <?php echo $sv['sinhvien']; ?> </td>
-                <td> <?php echo $sv['giotinh']; ?> </td>
-                <td> <?php echo $sv['mssv']; ?> </td>
-            </tr>
-        <?php } ?>
-    </table>
-</body>
-</html>
+    <?php } ?>
+</table>
